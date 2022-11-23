0cf70607dec58ea13ebe097fbaf03563433dadac
dg314 <david_grossman@brown.edu>
2022-11-23 12:37:37 -0500

    input size

--- components/SearchBar.tsx
+++ components/SearchBar.tsx
@@ -32,6 +32,8 @@ export default function SearchBar(props: SearchBarProps) {
     }
   }
 
+  const showArrow = text && text !== initialText;
+
   return (
     <div className="relative flex w-full max-w-2xl">
       <input 
@@ -39,12 +41,13 @@ export default function SearchBar(props: SearchBarProps) {
         onChange={e => setText(e.target.value)}
         onKeyDown={handleInputKeyDown}
         placeholder="Research anything..." 
-        className={`w-full bg-neutral-100 border-black outline-none rounded-full placeholder-neutral-500 text-black ${big ? "pl-12 pr-[4.5rem] py-3 text-lg" : "pl-9 pr-[4rem] py-2 text-base"}`}
+        size={30}
+        className={`w-full bg-neutral-100 border-black outline-none rounded-full placeholder-neutral-500 text-black ${big ? `pl-12 ${showArrow ? "pr-[4.5rem]" : "pr-6"} py-3 text-lg` : `pl-9 ${showArrow ? "pr-[4rem]" : "pr-5"} py-2 text-base`}`}
       />
       <div className={`absolute flex items-center top-0 h-full text-black pointer-events-none ${big ? "left-4 text-xl" : "left-3 text-lg"}`}>
         <AiOutlineSearch className="text-black" />
       </div>
-      {text && text !== initialText && <div className={`absolute flex items-center top-0 right-0 h-full text-black ${big ? "p-2 text-3xl" : "p-[0.35rem] text-2xl"}`}>
+      {showArrow && <div className={`absolute flex items-center top-0 right-0 h-full text-black ${big ? "p-2 text-3xl" : "p-[0.35rem] text-2xl"}`}>
         <Link
           href={resultsPath}
           className={`flex justify-center items-center bg-emerald-500 hover:bg-emerald-600 rounded-full h-full ${big ? "px-4" : "px-3"}`}
